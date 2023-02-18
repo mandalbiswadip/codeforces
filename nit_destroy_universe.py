d8ae32c8e9530e61ad7ca709fbe8b827a9f39ce3
Biswadip Mandal <biswadip.iitb@gmail.com>
2023-02-18 00:13:36 -0600

    1696 B

---
+++ nit_destroy_universe.py
@@ -0,0 +1,27 @@
+# 1686 B - NIT Destroys the Universe
+
+
+def destroy_count(a, n):
+    # count positive subarrays
+    count = 0
+
+    if a[0] != 0:
+        count += 1
+
+    for i in range(1,n):
+        if a[i-1] == 0 and a[i] != 0:
+            count += 1
+            if count > 2:
+                return 2
+    return count
+
+
+t = int(input().strip())
+
+for _ in range(t):
+    n = int(input().strip())
+    a = list(map(int, input().rstrip().split()))
+
+    ans = destroy_count(a, n)
+
+    print(ans)
